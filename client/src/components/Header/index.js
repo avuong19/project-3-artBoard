8d6e5ed93d150154b4fb7981a6c89899515a898c
macbeth <layne.wegenast@gmail.com>
2022-07-14 11:12:58 -0500

    added assets and css

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <header>
         <h1>
-            ART SITE
+            ARTBOARD
         </h1>
         <nav>
            <Link to="/login">LOGIN</Link> 
