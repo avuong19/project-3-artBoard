5a74f9c4478f54bf78396f007fd7762cac629807
Anh Vuong <vta19797@gmail.com>
2022-07-14 21:47:29 -0500

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose=require ('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/art-web', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/art-board', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
