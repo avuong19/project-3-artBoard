51f5506365796f7046f27b6654019031cdea393a
Anh Vuong <vta19797@gmail.com>
2022-07-15 20:18:06 -0500

    reset mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose=require ('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/art-board', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb:/127.0.0.1/art-board', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
