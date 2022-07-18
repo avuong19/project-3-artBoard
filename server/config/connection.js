d625a487e5408c4a5e480796222f1bd60c322b95
macbeth <layne.wegenast@gmail.com>
2022-07-17 19:53:06 -0500

    trying new uri

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose=require ('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/art-board', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/art-board', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   });
 
-module.exports=mongoose.connection
+module.exports=mongoose.connection;
